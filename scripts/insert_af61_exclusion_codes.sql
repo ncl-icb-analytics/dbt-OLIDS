993a6a1675321ffa784747c068c638c53c647cd4
EddieDavison92 <143042680+EddieDavison92@users.noreply.github.com>
2025-06-20 19:43:08 +0100

    removed redundant scripts and updated project file

--- scripts/insert_af61_exclusion_codes.sql
+++
@@ -1,7 +0,0 @@
--- Insert hardcoded exclusion codes for AF_61 into LTC_LCS_CODES table
-INSERT INTO DATA_LAB_NCL_TRAINING_TEMP.CODESETS.LTC_LCS_CODES 
-(CLUSTER_ID, CLUSTER_DESCRIPTION, SNOMED_CODE, SNOMED_DESCRIPTION)
-VALUES 
-('AF61_EXCLUSIONS', 'AF_61 Case Finding Exclusion Conditions', '1119304009', 'Long-haul COVID-19'),
-('AF61_EXCLUSIONS', 'AF_61 Case Finding Exclusion Conditions', '62067003', 'Hypoplastic left heart syndrome (disorder)'),
-('AF61_EXCLUSIONS', 'AF_61 Case Finding Exclusion Conditions', '132221000119109', 'Deep vein thrombosis'); 
