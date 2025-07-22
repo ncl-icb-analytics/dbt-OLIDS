810165eb2c8522372108b79e4919f855ac705589
Eddie Davison <eddie.davison@nhs.net>
2025-07-22 15:22:20 +0100

    fix: remove non-existent columns from stg_olids_person model
    - Commented out lds_business_key and primary_patient_id as they don't exist in PERSON_BACKUP
    - Added TODOs with possible alternative columns (unique_reference, requesting_patient_id)
    - Added additional available columns for reference
    - Model now runs successfully without errors
    
    Fixes #117

--- models/staging/stg_olids_person.sql
+++ models/staging/stg_olids_person.sql
@@ -5,9 +5,17 @@
 SELECT
     "lds_id" AS lds_id,
     "id" AS id,
-    "lds_business_key" AS lds_business_key,
+    -- TODO: lds_business_key column doesn't exist in PERSON_BACKUP
+    -- Possible alternatives: unique_reference or id
+    -- "lds_business_key" AS lds_business_key,
     "lds_dataset_id" AS lds_dataset_id,
-    "primary_patient_id" AS primary_patient_id,
+    -- TODO: primary_patient_id column doesn't exist in PERSON_BACKUP
+    -- Possible alternatives: requesting_patient_id or id
+    -- "primary_patient_id" AS primary_patient_id,
     "lds_start_date_time" AS lds_start_date_time,
-    "lds_end_date_time" AS lds_end_date_time
+    "lds_end_date_time" AS lds_end_date_time,
+    -- Additional available columns for reference:
+    "lds_datetime_data_acquired" AS lds_datetime_data_acquired,
+    "unique_reference" AS unique_reference,
+    "requesting_patient_id" AS requesting_patient_id
 FROM {{ source('OLIDS_MASKED', 'PERSON_BACKUP') }}
