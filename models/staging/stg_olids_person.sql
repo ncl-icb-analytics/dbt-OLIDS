cc3d51d9d45cc21262fcaac199e3c84a23925339
Eddie Davison <eddie.davison@nhs.net>
2025-07-28 10:01:15 +0100

    fix: update stg_olids_person to handle missing columns in PERSON table
    - Comment out missing columns (lds_business_key, primary_patient_id) with TODO notes
    - Add alternative columns that exist in the table (unique_reference, requesting_patient_id)
    - Include lds_datetime_data_acquired for data freshness tracking
    - Update sources.yml to reflect actual PERSON table structure

--- models/staging/stg_olids_person.sql
+++ models/staging/stg_olids_person.sql
@@ -1,15 +1,14 @@
--- Staging model for OLIDS_MASKED.PERSON_BACKUP
+-- Staging model for OLIDS_MASKED.PERSON
 -- Source: "Data_Store_OLIDS_UAT".OLIDS_MASKED
--- Using PERSON_BACKUP as PERSON table was deleted
 
 SELECT
     "lds_id" AS lds_id,
     "id" AS id,
-    -- TODO: lds_business_key column doesn't exist in PERSON_BACKUP
+    -- TODO: lds_business_key column doesn't exist in PERSON
     -- Possible alternatives: unique_reference or id
     -- "lds_business_key" AS lds_business_key,
     "lds_dataset_id" AS lds_dataset_id,
-    -- TODO: primary_patient_id column doesn't exist in PERSON_BACKUP
+    -- TODO: primary_patient_id column doesn't exist in PERSON
     -- Possible alternatives: requesting_patient_id or id
     -- "primary_patient_id" AS primary_patient_id,
     "lds_start_date_time" AS lds_start_date_time,
@@ -18,4 +17,4 @@ SELECT
     "lds_datetime_data_acquired" AS lds_datetime_data_acquired,
     "unique_reference" AS unique_reference,
     "requesting_patient_id" AS requesting_patient_id
-FROM {{ source('OLIDS_MASKED', 'PERSON_BACKUP') }}
+FROM {{ source('OLIDS_MASKED', 'PERSON') }}
